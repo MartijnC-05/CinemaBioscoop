efad218c99624735a878f46eac5b675f24f0e0f1
MartijnC-05 <110739@st-maartenscollege.nl>
2022-02-11 23:12:05 +0100

    animatie op informatie banner

--- CinemaBioscoop/wwwroot/js/site.js
+++ CinemaBioscoop/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var slideIndex = 0;
+showSlides();
 
-// Write your JavaScript code.
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" actief", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " actief";
+    setTimeout(showSlides, 10000); // Change image every 2 seconds
+}
