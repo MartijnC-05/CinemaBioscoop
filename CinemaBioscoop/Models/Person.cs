e61c6a41633e9c8c2514c1409e076ee0c99730b4
Martijn <110739@st-maartenscollege.nl>
2022-03-16 11:39:31 +0100

    contact page styling

--- CinemaBioscoop/Models/Person.cs
+++ CinemaBioscoop/Models/Person.cs
@@ -4,11 +4,19 @@ namespace CinemaBioscoop.Models
 {
     public class Person
     {
-        [Required]
+        [Required (ErrorMessage ="Gelieve uw voornaam in te vullen")]
         public string Voornaam { get; set; }
+
+        [Required(ErrorMessage = "Gelieve uw achternaam in te vullen")]
         public string Achternaam { get; set; }
+
+        [Required(ErrorMessage = "Gelieve uw emailadres in te vullen")]
+        [EmailAddress(ErrorMessage = "Gelieve een geldig emailadres in te vullen")]
         public string Email { get; set; }
-        public string Telefoon { get; set; }
+
+        public string? Telefoon { get; set; }
+
+        [Required(ErrorMessage = "Gelieve een bericht achter te laten")]
         public string Bericht { get; set; }
 
     }
