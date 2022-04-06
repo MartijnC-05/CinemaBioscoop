174358e337b4251d1187c3c0227cfcb8816f023c
Pleun B <110772@st-maartenscollege.nl>
2022-04-06 11:38:13 +0200

    database added (werken met een class)

--- CinemaBioscoop/Controllers/HomeController.cs
+++ CinemaBioscoop/Controllers/HomeController.cs
@@ -132,20 +132,35 @@ namespace CinemaBioscoop.Controllers
         [Route("Database")]
         public IActionResult Database()
         {
-            //Alle producten ophalen
+            //lijst met producten ophalen
+            var products = GetAllProducts();
+
+            //Lijst met producten in de html stoppen
+            return View(products);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            // alle producten ophalen uit de database
             var rows = DatabaseConnector.GetRows("select * from product");
 
-            //Lijst maken om alle namen in te stoppen
-            List<string> names = new List<string>();
+            //lijst maken om alle producten in te stoppen
+            List<Product> products = new List<Product>();
 
             foreach (var row in rows)
             {
-                //elke naam toevoegen aan lijst
-                names.Add(row["naam"].ToString());
+                //voor elke rij maken we nu een product
+                Product p = new Product();
+                p.Naam = row["naam"].ToString();
+                p.Prijs = row["prijs"].ToString();
+                p.Beschikbaarheid = Convert.ToInt32(row["beschikbaarheid"]);
+                p.Id = Convert.ToInt32(row["id"]);
+
+                //en dat product voegen we toe aan de lijst met producten
+                products.Add(p);
             }
 
-            //de lijst met namen in html
-            return View(names);
+            return products;
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
